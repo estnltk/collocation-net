39182c686ba367600152421e5dd5169f1e5a86c7
Birgit <sormusbirgit@gmail.com>
2021-11-05 11:56:57 +0200

    some additional implementations for collocation net predict

--- collocation_net.py
+++ collocation_net.py
@@ -1,7 +1,9 @@
 import pickle
 import scipy
+import os
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 from sklearn.neighbors import NearestNeighbors
 
 
@@ -14,70 +16,54 @@ class CollocationNet:
     CollocationNet class
     """
 
-    def __init__(self, model: str = 'LDA', data: str = "data/df.csv"):
-        self.data = pd.read_csv(data, index_col=0)
-        self.noun_dist = np.load(f"data/{model.lower()}_noun_distribution.npy")
-        self.adj_dist = np.load(f"data/{model.lower()}_adj_distribution.npy")
+    def __init__(self, collocation_type: str = 'noun_adjective', read_data: bool = False):
+        path = f"{os.path.dirname(os.path.abspath(__file__))}/data/{collocation_type}"
+        # path = f"data/{collocation_type}"
+        if read_data:
+            self.data = pd.read_csv(f"{path}/df.csv", index_col=0)
+        self.noun_dist = np.load(f"{path}/lda_noun_distribution.npy")
+        self.adj_dist = np.load(f"{path}/lda_adj_distribution.npy")
+        self.nouns = np.load(f"{path}/nouns.npy", allow_pickle=True)
+        self.adjectives = np.load(f"{path}/adjectives.npy", allow_pickle=True)
 
-        # with open(f"{model.lower()}_model.pickle", "rb") as f:
-        #     self.model = pickle.load(f)
+        self.adj_dist_probs = self.adj_dist / self.adj_dist.sum(axis=0)
 
-        with open(f"data/{model.lower()}_topics.pickle", "rb") as f:
+        with open(f"{path}/lda_topics.pickle", "rb") as f:
             self.topics = pickle.load(f)
 
     def noun_index(self, word: str) -> int:
-        for i, noun in enumerate(self.data.index):
+        for i, noun in enumerate(self.nouns):
             if noun == word:
                 return i
-        return -1
+        raise CollocationNetException(f"Word '{word}' not in the dataset.")
 
     def adjective_index(self, word: str) -> int:
-        for i, adj in enumerate(self.data.columns):
+        for i, adj in enumerate(self.adjectives):
             if adj == word:
                 return i
-        return -1
+        raise CollocationNetException(f"Word '{word}' not in the dataset.")
 
-    def get_nouns_used_with(self, word: str, number_of_words: int = 10) -> list:
-        # if word not in self.data.columns:
-        #     raise CollocationNetException(f"Word '{word}' not in dataset")
-        # return self.data[word].sort_values(ascending=False).index[:number_of_words].values.tolist()
-
-        word_index = None
-
-        for i, adj in enumerate(self.data.columns):
-            if adj == word:
-                word_index = i
-
-        if word_index is None:
-            raise CollocationNetException(f"Word '{word}' not in dataset")
+    def nouns_used_with(self, word: str, number_of_words: int = 10) -> list:
+        word_index = self.adjective_index(word)
 
         adj_topic = self.adj_dist[:, word_index].argmax()
         noun_topic = self.noun_dist[:, adj_topic].argsort()[::-1][:number_of_words]
 
-        return self.data.index[noun_topic].tolist()
+        return self.nouns[noun_topic].tolist()
 
-    def get_adjectives_used_with(self, word: str, number_of_words: int = 10) -> list:
-        # threshold ka (+ eelmisele) - lisada ise threshold, mis väärtusega minimaalselt võivad olla
-        # sarnased sõnad vms, mõtle kas on pointi
-        # if word not in self.data.index:
-        #     raise CollocationNetException(f"Word '{word}' not in dataset")
-        # return self.data.loc[word].sort_values(ascending=False).index[:number_of_words].values.tolist()
-
+    def adjectives_used_with(self, word: str, number_of_words: int = 10) -> list:
         word_index = self.noun_index(word)
-
-        if word_index == -1:
-            raise CollocationNetException(f"Word '{word}' not in dataset")
 
         noun_topic = self.noun_dist[word_index].argmax()
         adj_topic = self.adj_dist[noun_topic].argsort()[::-1][:number_of_words]
 
-        return self.data.columns[adj_topic].tolist()
+        return self.adjectives[adj_topic].tolist()
 
-    def get_similar_nouns(self, word: str, number_of_words: int = 10) -> list:
+    def similar_nouns(self, word: str, number_of_words: int = 10) -> list:
         knn = NearestNeighbors(n_neighbors=number_of_words + 1).fit(self.noun_dist)
         idx_of_word = None
 
-        for i, noun in enumerate(self.data.index):
+        for i, noun in enumerate(self.nouns):
             if noun == word:
                 idx_of_word = i
 
@@ -85,15 +71,15 @@ class CollocationNet:
             raise CollocationNetException(f"Word '{word}' not in dataset")
 
         neighbour_ids = knn.kneighbors(self.noun_dist[idx_of_word].reshape(1, -1), return_distance=False)
-        neighbours = self.data.index.to_numpy()[neighbour_ids]
+        neighbours = self.nouns[neighbour_ids]
 
         return list(neighbours[0])[1:]
 
-    def similar_noun_for_list(self, words: list) -> str:
+    def similar_nouns_for_list(self, words: list, num_of_words: int = 10) -> list:
         similar_for_each = {}
 
         for word in words:
-            word_similar = self.get_similar_nouns(word, self.data.index.shape[0]-1)
+            word_similar = self.similar_nouns(word, self.nouns.shape[0] - 1)
             similar_for_each[word] = word_similar
 
         common = []
@@ -118,14 +104,16 @@ class CollocationNet:
         if len(sorted_common) == 0:
             return None
 
-        return sorted_common[0][0]
+        similar_words = [sort[0] for sort in sorted_common]
 
-    def get_similar_adjectives(self, word: str, number_of_words: int = 10) -> list:
+        return similar_words[:num_of_words]
+
+    def similar_adjectives(self, word: str, number_of_words: int = 10) -> list:
         adj_vals = self.adj_dist.T
         knn = NearestNeighbors(n_neighbors=number_of_words + 1).fit(adj_vals)
         idx_of_word = None
 
-        for i, adj in enumerate(self.data.columns):
+        for i, adj in enumerate(self.adjectives):
             if adj == word:
                 idx_of_word = i
 
@@ -133,15 +121,15 @@ class CollocationNet:
             raise CollocationNetException(f"Word '{word}' not in dataset")
 
         neighbour_ids = knn.kneighbors(adj_vals[idx_of_word].reshape(1, -1), return_distance=False)
-        neighbours = self.data.columns.to_numpy()[neighbour_ids]
+        neighbours = self.adjectives[neighbour_ids]
 
         return list(neighbours[0])[1:]
 
-    def similar_adjective_for_list(self, words: list) -> str:
+    def similar_adjectives_for_list(self, words: list, num_of_words: int = 10) -> list:
         similar_for_each = {}
 
         for word in words:
-            word_similar = self.get_similar_adjectives(word, self.data.columns.shape[0]-1)
+            word_similar = self.similar_adjectives(word, self.adjectives.shape[0] - 1)
             similar_for_each[word] = word_similar
 
         common = []
@@ -166,7 +154,9 @@ class CollocationNet:
         if len(sorted_common) == 0:
             return None
 
-        return sorted_common[0][0]
+        similar_words = [sort[0] for sort in sorted_common]
+
+        return similar_words[:num_of_words]
 
     def topic(self, word: str) -> int:
         """
@@ -184,9 +174,10 @@ class CollocationNet:
 
     def characterisation(self, word: str, number_of_topics: int = 10, number_of_adjs: int = 10):
         """
-        TODO
-        see tabel??
+
         :param word:
+        :param number_of_topics:
+        :param number_of_adjs:
         :return:
         """
         word_index = self.noun_index(word)
@@ -196,7 +187,7 @@ class CollocationNet:
 
         topic_vector = self.noun_dist[word_index]
         sorted_index = topic_vector.argsort()[::-1][:number_of_topics]
-        adjs_in_topics = pd.DataFrame(self.adj_dist, columns=self.data.columns)
+        adjs_in_topics = pd.DataFrame(self.adj_dist, columns=self.adjectives)
         final_list = list()
 
         for i in sorted_index:
@@ -204,19 +195,27 @@ class CollocationNet:
 
         return final_list
 
-    def predict(self, noun: str, adjectives: list) -> list:
+    def predict(self, noun: str, adjectives: list = None, num_of_adjectives: int = 10) -> list:
         """
         TODO
 
         :param noun:
         :param adjectives:
+        :param num_of_adjectives:
         :return:
         """
         noun_index = self.noun_index(noun)
-        if noun_index == -1:
-            raise CollocationNetException(f"Noun '{noun}' is not in the dataset.")
-
         noun_topics = self.noun_dist[noun_index]
+
+        avg_per_adj = np.matmul(noun_topics, self.adj_dist_probs)
+
+        if adjectives is None:
+            top_adj_ind = np.argpartition(avg_per_adj, -num_of_adjectives)[-num_of_adjectives:]
+            top_adj_ind = top_adj_ind[np.argsort(avg_per_adj[top_adj_ind])][::-1]
+            top_probs = avg_per_adj[top_adj_ind]
+            top_adjs = self.adjectives[top_adj_ind]
+            return list(zip(*(top_adjs, top_probs)))
+
         results = []
 
         for adj in adjectives:
@@ -225,61 +224,58 @@ class CollocationNet:
             if adj_idx == -1:
                 continue
 
-            adj_dirichlet = scipy.stats.dirichlet.rvs(self.adj_dist[:, adj_idx], size=1000)
-            prob = np.mean(np.matmul(noun_topics, adj_dirichlet.T))
-            results.append((adj, prob))
+            results.append((adj, avg_per_adj[adj_idx]))
 
         return sorted(results, key=lambda x: x[1], reverse=True)
 
-    def predict_for_several(self, words: str):
-        char_for_each = {}
+    def predict_for_several(self, words: list, num_of_adjectives: int = 10):
+        # avg_probs = defaultdict(lambda:1)
+        avg_probs = defaultdict(int)
 
         for word in words:
-            characterisation = self.characterisation(word, number_of_topics=self.noun_dist[0].shape[0])
-            char_for_each[word] = characterisation
+            adj_probs = self.predict(word, num_of_adjectives=self.adjectives.shape[0])
+            for adj, prob in adj_probs:
+                avg_probs[adj] += prob
 
-        sorted_for_each = {}
+        avg_probs = [(k, v / len(words)) for k, v in avg_probs.items()]
 
-        for word, char in char_for_each.items():
-            for c in char:
-                for adj in c[1]:
-                    if word not in sorted_for_each:
-                        sorted_for_each[word] = [adj]
-                    else:
-                        sorted_for_each[word].append(adj)
+        return sorted(avg_probs, key=lambda x: x[1], reverse=True)[:num_of_adjectives]
 
-        common = []
+    def predict_topic_for_several(self, words: list, num_of_topics: int = 10, num_of_adjectives: int = 10):
+        """
+        When given a list of several nouns, it predicts the most likely topic for them and returns
+        the average probability of that topic and the top adjectives describing that topic.
+        :param words:
+        :param num_of_topics:
+        :param num_of_adjectives:
+        :return:
+        """
+        avg_probs = defaultdict(int)
 
-        for i, adjective in enumerate(sorted_for_each[words[0]]):
-            is_in_all = sum([1 for l in words[1:] if adjective in sorted_for_each[l]])
-            if is_in_all != len(words) - 1:
-                continue
+        for word in words:
+            word_idx = self.noun_index(word)
+            topic_probs = self.noun_dist[word_idx]
+            for i, prob in enumerate(topic_probs):
+                avg_probs[i] += prob
 
-            c = [adjective, i]
+        avg_probs = [(k, v / len(words)) for k, v in avg_probs.items()]
+        sorted_topics = sorted(avg_probs, key=lambda x: x[1], reverse=True)[:num_of_topics]
+        predicted_topics = []
 
-            for l in words[1:]:
-                c.append(sorted_for_each[l].index(adjective))
+        for topic_id, topic_prob in sorted_topics:
+            topic_adjs = self.adj_dist[topic_id]
+            top_adj_ind = np.argpartition(topic_adjs, -num_of_adjectives)[-num_of_adjectives:]
+            top_adj_ind = top_adj_ind[np.argsort(topic_adjs[top_adj_ind])][::-1]
+            predicted_topics.append((list(self.adjectives[top_adj_ind]), topic_prob))
 
-            common.append(c)
-
-        sorted_common = sorted(common, key=lambda x: sum(x[1:]))
-
-        if len(sorted_common) == 0:
-            return None
-
-        return sorted_common[0][0]
+        return predicted_topics
 
     def usable_phrase(self, noun: str, adj: str, number_of_topics: int = 10) -> str:
         """
-        TODO
-        NT KOLLANE ESMASPÄEV - ENNUSTA KAS ON OK VÕI EI
-        vt seda järjest nt esimeses top klastris esmaspäev jaoks kui on
 
-        võta näiteks top 10 klastrit ja siis iga klastri jaoks sellised omadussõnad, et
-        väärtused oleks kõigil min 100x väiksemad suurimast vms, kui nende hulgas pole
-        kuskil sõna, siis ei ole kasutatav, otherwise on
-
-        :param word:
+        :param noun:
+        :param adj:
+        :param number_of_topics:
         :return:
         """
         word_index = self.noun_index(noun)
@@ -289,7 +285,7 @@ class CollocationNet:
 
         adj_index = None
 
-        for i, adj_in_df in enumerate(self.data.columns):
+        for i, adj_in_df in enumerate(self.adjectives):
             if adj_in_df == adj:
                 adj_index = i
 
@@ -298,7 +294,7 @@ class CollocationNet:
 
         topic_vector = self.noun_dist[word_index]
         sorted_index = topic_vector.argsort()[::-1][:number_of_topics]
-        adjs_in_topics = pd.DataFrame(self.adj_dist, columns=self.data.columns)
+        adjs_in_topics = pd.DataFrame(self.adj_dist, columns=self.adjectives)
 
         for i in sorted_index:
             adjs = adjs_in_topics.loc[i][adjs_in_topics.loc[i] > 10].sort_values(ascending=False).index.tolist()
