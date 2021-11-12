dfd3a749a3410773d3bc5a91dfa45d3b796f0755
Birgit <sormusbirgit@gmail.com>
2021-11-12 02:23:18 +0200

    reorganised the structure and created a separate class for noun-adjective collocation net

--- tests/test_collocation_net.py
+++ tests/test_collocation_net.py
@@ -1,41 +1,42 @@
 import pytest
-import sys, os
+# import sys, os
 # from collocation_net import CollocationNet, CollocationNetException
 # from .. import collocation_net
 # from collocation_net import CollocationNet, CollocationNetException
-sys.path.append(os.path.abspath('../'))  # temporary fix to see if tests work
-from collocation_net import CollocationNet, CollocationNetException
+# sys.path.append(os.path.abspath('../'))  # temporary fix to see if tests work
+# from collocation_net import CollocationNet, CollocationNetException
+from collocation_net.collocation_net import BaseCollocationNet, CollocationNetException
 
-cn = CollocationNet()
+cn = BaseCollocationNet()
 
 
 def test_unknown_word():
     with pytest.raises(CollocationNetException):
-        index = cn.noun_index('testsõna')
+        index = cn.row_index('testsõna')
 
 
 def test_predict():
-    pred = cn.predict("kohv", ["tugev", "kange"])
+    pred = cn.predict_column_probabilities("kohv", ["tugev", "kange"])
     assert isinstance(pred[0][0], str)
     assert isinstance(pred[0][1], float)
     assert pred[0][0] == "kange"
 
 
 def test_possible_collocates_output():
-    nouns = cn.nouns_used_with("kange", 5)
-    adjectives = cn.adjectives_used_with("kohv", 5)
+    nouns = cn.rows_used_with("kange", 5)
+    adjectives = cn.columns_used_with("kohv", 5)
     assert len(nouns) == 5
     assert len(adjectives) == 5
 
 
 def test_similar_nouns():
-    similar_nouns = cn.similar_nouns("kohv", 1)
-    assert similar_nouns[0] in cn.nouns
+    similar_nouns = cn.similar_rows("kohv", 1)
+    assert similar_nouns[0] in cn.rows
 
 
 def test_similar_adjectives():
-    similar_adjectives = cn.similar_adjectives("kange", 1)
-    assert similar_adjectives[0] in cn.adjectives
+    similar_adjectives = cn.similar_columns("kange", 1)
+    assert similar_adjectives[0] in cn.columns
 
 
 def test_topic():
@@ -58,7 +59,7 @@ def test_characterisation():
 
 
 def test_predict_for_several():
-    pred = cn.predict_for_several(["kohv", "tee"], 1)
+    pred = cn.predict_for_several_rows(["kohv", "tee"], 1)
     assert len(pred) == 1
     pred = pred[0]
     assert isinstance(pred[0], str)
@@ -66,7 +67,7 @@ def test_predict_for_several():
 
 
 def test_predict_topic_for_several():
-    pred = cn.predict_topic_for_several(["kohv", "tee"], 1)
+    pred = cn.predict_topic_for_several_rows(["kohv", "tee"], 1)
     assert len(pred) == 1
     pred = pred[0]
     assert isinstance(pred[0], list)
